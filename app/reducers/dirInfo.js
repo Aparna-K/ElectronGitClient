210b2a094b770d83e3010b3f75c1039df1aba578
Aparna Korattyswaroopam <aparna@binarytwine.com>
2016-09-15 14:41:02 +0530

    Reducer return value correction. The return hash does not need the key if the name of the reducer function is same as the key

--- app/reducers/dirInfo.js
+++ app/reducers/dirInfo.js
@@ -3,12 +3,7 @@ import {GET_DIR_INFO} from '../actions/actionCreators';
 export default function dirInfo(state = [], action) {
   switch (action.type) {
     case GET_DIR_INFO:
-      let new_state =
-        {
-          dirInfo: dirTreeHelpers.getFiles(action.path)
-        };
-      console.log(new_state);
-      return new_state;
+      return dirTreeHelpers.getFiles(action.path);
     default:
       return state;
   }
