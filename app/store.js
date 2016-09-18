bcda7911783402aee5f721eda3c4724f76f8ce61
Aparna Korattyswaroopam <aparna@binarytwine.com>
2016-09-18 17:42:17 +0530

    Have async functions for file i/o - The reducers haven't begun using them yet. The initial state does not have default values

--- app/store.js
+++ app/store.js
@@ -1,16 +1,29 @@
 import { createStore, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-function setDefaultState(){
-  let currentDir = "./";
-  return (
-      {
-        dirInfo: dirTreeHelpers.getFiles(currentDir)
-      }
-  );
-}
-const defaultState = setDefaultState();
+// function setDefaultState(){
+//   let currentDir = "./",
+//       dirInfo;
+//   dirTreeHelpers.getFilesInfoAsync(currentDir).then((files => dirInfo = files));
+//   return { dirInfo: dirInfo }
+// }
+// const defaultState = setDefaultState();
+// const store = createStore(rootReducer, defaultState);
 
-const store = createStore(rootReducer, defaultState);
+var currentDir = "./",
+    store=createStore(rootReducer, {}),
+    defaultState;
+
+(() => {
+  dirTreeHelpers.getFilesInfoAsync(currentDir)
+      .then((filesInfo) => {
+        return {dirInfo: filesInfo}
+      })
+      .then((defaultState) => {
+        store = createStore(rootReducer, defaultState);
+      })
+      .catch((err) => { throw err });
+})();
+
 
 export default store;
--- helperMethods/dirTreeHelpersRenderer.js
+++ helperMethods/dirTreeHelpersRenderer.js
@@ -1,6 +1,78 @@
 var fs = require("fs");
+var async = require("async");
 
-function getFiles(path) {
+
+/*
+    Args:
+      path => (String) path to a directory.
+    Return:
+      A Promise object;
+      resolves => Listing of the directory in <path>
+      rejects  => Error
+*/
+function getDirListingAsync(path){
+  return new Promise(function(resolve, reject){
+    fs.readdir(path, (err, files) => {
+      if(err){
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    })
+  })
+}
+
+/*
+    Args:
+      files => (Array) array of file paths
+    Return:
+      A Promise object;
+      resolves => An array of file stats corresponding to the input <files>
+      rejects  => Error
+
+*/
+function getFstatListAsync(files = []){
+  return new Promise(function(resolve, reject){
+    async.map(files, fs.stat, (err, file_stat_arr) => {
+      if(err){
+        reject(err);
+      } else {
+        resolve(file_stat_arr);
+      }
+    });
+  });
+}
+
+/*
+    Args:
+      path => (String) path to a directory
+    Return:
+      A promise object:
+      resolves => Array of javascript objects of the signature
+        [{"name": <fileName>, "isDir": <boolean>}, {...}, ...]
+      rejects  => Error
+*/
+
+function getFilesInfoAsync(path){
+  let filesInDir = [];
+  return getDirListingAsync(path)
+          .then((files) => {
+            filesInDir = files;
+            return getFstatListAsync(files.map((f) => {return path + "/" + f}));
+          })
+          .then((file_stat_arr) => {
+            var files_hash = file_stat_arr.map((file_info, i) => {
+              return {
+                "name": filesInDir[i],
+                "isDir": file_info.isDirectory()
+              }
+            });
+            return files_hash;
+          })
+          .catch((err) => { throw err })
+}
+
+function getFilesSync(path) {
   let files = fs.readdirSync(path);
   let files_info = files.map( (file) => {
     return ({
@@ -11,11 +83,12 @@ function getFiles(path) {
   return files_info;
 }
 
-function getFileType(path) {
+function getFileTypeSync(path) {
   return fs.statSync(path).isDirectory() ? "Dir" : "File"
 }
 
 module.exports = {
-  getFiles: getFiles,
-  getFileType: getFileType
+  getFiles: getFilesSync,
+  getFileType: getFileTypeSync,
+  getFilesInfoAsync: getFilesInfoAsync
 };
